582a3021bcceb8b253b129a685eef93afe056a8a
Lauro Santana <devlauross@gmail.com>
2024-04-10 21:47:48 -0300

    Tests

---
+++ 01_dataTypes/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var m = "Hello Wolrd";
+var i = 1;
+var num = 1.1;
+console.log(i, num);
---
+++ 01_dataTypes/main.ts
@@ -0,0 +1,8 @@
+export {}
+
+let m:string = "Hello Wolrd";
+
+let i:number = 1;
+let num:number = 1.1;
+
+console.log(i, num);
