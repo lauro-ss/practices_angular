5c922356afc5ea027afc4e0341af8af8ae7ce1e8
Lauro Santana <devlauross@gmail.com>
2024-04-11 00:34:08 -0300

    new types

--- 01_dataTypes/main.js
+++ 01_dataTypes/main.js
@@ -1,6 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var m = "Hello Wolrd";
-var i = 1;
-var num = 1.1;
-console.log(i, num);
+var i = -39;
+var num = 1.1132132123;
+var b = true;
+var a = [1, 2];
+var n = undefined;
+var list = [1, "Lauro"];
+console.log(list);
+var Color;
+(function (Color) {
+    Color[Color["Blue"] = 0] = "Blue";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Orange"] = 2] = "Orange";
+})(Color || (Color = {}));
+var color = Color.Blue;
+console.log(color);
+var name = "Lauro Santana";
+console.log(name.toLowerCase());
+var f = function (t) { return t.name; };
+var o = {
+    name: "Lauro"
+};
+console.log(f(o));
+var p = {
+    id: 1,
+    name: "LauroOOUO",
+    lastName: "Santana"
+};
+console.log(f(p));
--- 01_dataTypes/main.ts
+++ 01_dataTypes/main.ts
@@ -1,8 +1,52 @@
 export {}
 
-let m:string = "Hello Wolrd";
+let m: string = "Hello Wolrd";
 
-let i:number = 1;
-let num:number = 1.1;
+let i: number = -39;
+let num: number = 1.1132132123;
 
-console.log(i, num);
+let b: boolean = true;
+
+const a = [1,2];
+
+let n:any = undefined;
+
+var list: [number, string] = [1, "Lauro"];
+
+console.log(list)
+
+enum Color{Blue, Red, Orange}
+
+let color = Color.Blue;
+
+console.log(color)
+
+let name: unknown = "Lauro Santana";
+
+console.log((name as string).toLowerCase())
+
+interface tag {
+    name: string
+}
+
+const f = function(t: tag): string { return t.name }
+const o = {
+    name: "Lauro"
+}
+
+console.log(f(o))
+
+type Person = {
+    id: number;
+    name: string;
+    lastName: string
+}
+
+
+let p: Person = {
+    id: 1,
+    name: "LauroOOUO",
+    lastName: "Santana"
+}
+
+console.log(f(p))
